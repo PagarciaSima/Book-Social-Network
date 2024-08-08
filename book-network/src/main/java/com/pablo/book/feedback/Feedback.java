2b715a2b662ca4d08030e13d7e1ebf1534aa03ac
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-08-08 16:52:19 +0200

    Defining entities

---
+++ book-network/src/main/java/com/pablo/book/feedback/Feedback.java
@@ -0,0 +1,21 @@
+package com.pablo.book.feedback;
+
+import com.pablo.book.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    private Double note;
+    private String comment;
+}
---
+++ book-network/src/main/java/com/pablo/book/history/BookTransactionHistory.java
@@ -0,0 +1,26 @@
+package com.pablo.book.history;
+
+import com.pablo.book.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    // user relationship
+
+    // book relationship
+
+    private boolean returned;
+    private boolean returnApproved;
+    
+}
