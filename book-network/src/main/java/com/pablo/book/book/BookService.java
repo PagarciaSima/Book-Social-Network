6c91590c3652fafd01ae34ec12cb69585c11d215
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-08-08 18:23:39 +0200

    Post book

---
+++ book-network/src/main/java/com/pablo/book/book/BookService.java
@@ -0,0 +1,21 @@
+package com.pablo.book.book;
+
+import com.pablo.book.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookMapper bookMapper;
+    private final BookRepository bookRepository;
+
+    public Integer save(BookRequest request, Authentication connectedUser) {
+        User user = ((User) connectedUser.getPrincipal());
+        Book book = bookMapper.toBook(request);
+        book.setOwner(user);
+        return bookRepository.save(book).getId();
+    }
+}
