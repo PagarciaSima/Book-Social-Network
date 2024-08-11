07cc8b5b8c6f1c578f6373a9aace6e097f24ff29
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-08-11 15:48:39 +0200

    Borrow book

--- book-network/src/main/java/com/pablo/book/history/BookTransactionHistoryRepository.java
+++ book-network/src/main/java/com/pablo/book/history/BookTransactionHistoryRepository.java
@@ -11,4 +11,9 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
 
     @Query("SELECT history FROM BookTransactionHistory history WHERE history.book.owner.id = :userId")
     Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Integer userId);
+
+    @Query("SELECT (COUNT(*) > 0) AS isBorrowed FROM BookTransactionHistory bookTransactionHistory WHERE " +
+            "bookTransactionHistory.user.id = :userId AND bookTransactionHistory.book.id = :bookId AND " +
+            "bookTransactionHistory.returnApproved = false")
+    boolean isAlreadyBorrowedByUser(Integer bookId, Integer userId);
 }
