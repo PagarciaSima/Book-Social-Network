08b20988c7388aaeedea38497e39249e9b27ab34
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-08-14 17:05:36 +0200

    Menu component angular

--- book-network-ui/src/app/app-routing.module.ts
+++ book-network-ui/src/app/app-routing.module.ts
@@ -5,9 +5,12 @@ import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'activate-account', component: ActivateAccountComponent }
+  { path: 'activate-account', component: ActivateAccountComponent },
+  { path: 'books', loadChildren: () => import ('./modules/book/book.module').then(m => m.BookModule) }
+
 
 ];
 
