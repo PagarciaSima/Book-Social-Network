9d5b3b45d237cb6d0a18094c57f3012eabb0f4a2
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-08-20 22:08:22 +0200

    Implement My Books Page

--- book-network-ui/src/app/modules/book/book-routing.module.ts
+++ book-network-ui/src/app/modules/book/book-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { BookListComponent } from './pages/book-list/book-list.component';
+import { MyBooksComponent } from './pages/my-books/my-books.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
     children: [
-      {path: '', component: BookListComponent}
-  ]}
+      { path: '', component: BookListComponent },
+      {path: 'my-books', component: MyBooksComponent}
+    ]
+  }
 ];
 
 @NgModule({
---
+++ book-network-ui/src/app/modules/book/pages/my-books/my-books.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BookResponse } from 'src/app/services/models/book-response';
+import { PageResponseBookResponse } from 'src/app/services/models/page-response-book-response';
+import { BookService } from 'src/app/services/services/book.service';
+
+@Component({
+  selector: 'app-my-books',
+  templateUrl: './my-books.component.html',
+  styleUrls: ['./my-books.component.scss']
+})
+export class MyBooksComponent implements OnInit{
+
+  bookResponse: PageResponseBookResponse = {};
+  public page: number = 0;
+  public size: number = 4;
+
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
+
+  findAllBooks() {
+    this.bookService.findALlBooksByOwner({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (books: PageResponseBookResponse) => {
+        this.bookResponse = books;
+        console.log("Response " + this.bookResponse.content)
+      }
+      , error: () => {
+
+      }
+    })
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+  
+  goToPreviousPage() {
+    this.page--;
+    this.findAllBooks();
+  }
+
+  goToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+  
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  get isLastPage(): boolean {
+    return this.page == this.bookResponse.totalPages as number - 1;
+  }
+
+  editBook(book: BookResponse) {
+
+  }
+  
+  shareBook(book: BookResponse) {
+      
+  }
+  
+  archiveBook(book: BookResponse) {
+
+  }
+}
