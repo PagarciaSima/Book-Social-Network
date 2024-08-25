0bccb6a40309ffb18bb95d2317c7b23742b12668
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-08-25 17:41:32 +0200

    Return book and approve return Angular

---
+++ book-network-ui/src/app/modules/book/pages/return-books/return-books.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { BorrowedBookResponse, PageResponseBorrowedBookResponse } from 'src/app/services/models';
+import { BookService } from 'src/app/services/services';
+
+@Component({
+  selector: 'app-return-books',
+  templateUrl: './return-books.component.html',
+  styleUrls: ['./return-books.component.scss']
+})
+export class ReturnBooksComponent implements OnInit{
+
+  returnedBooks: PageResponseBorrowedBookResponse = {};
+  page: number = 0;
+  size: number = 5;
+  message: string = '';
+  level: string = 'success';
+
+  constructor(
+    private bookService: BookService,
+  ) {}
+  
+  ngOnInit(): void {
+    this.findAllReturnedBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllReturnedBooks();
+  }
+
+  findAllReturnedBooks() {
+    this.bookService.findAllReturnedBooks({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (resp: PageResponseBorrowedBookResponse) => {
+        this.returnedBooks = resp;
+      }
+    })
+  }
+  
+  goToPreviousPage() {
+    this.page--;
+    this.findAllReturnedBooks();
+  }
+
+  goToPage(page: number) {
+    this.page = page;
+    this.findAllReturnedBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.returnedBooks.totalPages as number - 1;
+    this.findAllReturnedBooks();
+  }
+  
+  goToNextPage() {
+    this.page++;
+    this.findAllReturnedBooks();
+  }
+
+  get isLastPage(): boolean {
+    return this.page == this.returnedBooks.totalPages as number - 1;
+  }
+
+  approveBookReturn(book: BorrowedBookResponse) {
+    if (!book.returned) {
+      this.level = 'error';
+      this.message = 'The book is not returned yet';
+      return;
+    }
+    this.bookService.approveReturnBorrowBook({
+      'book-id': book.id as number
+    }).subscribe({
+      next: () => {
+        this.level = 'success';
+        this.message = 'Book return approved';
+        this.findAllReturnedBooks();
+      }
+    });
+  }
+}
--- book-network/src/main/java/com/pablo/book/book/BookService.java
+++ book-network/src/main/java/com/pablo/book/book/BookService.java
@@ -202,8 +202,8 @@ public class BookService {
                     "not shareable");
         }
         User user = ((User) connectedUser.getPrincipal());
-        if(Objects.equals(book.getOwner().getId(), user.getId())){
-            throw new OperationNotPermittedException("You cannot borrow or return your own book");
+        if(!Objects.equals(book.getOwner().getId(), user.getId())){
+            throw new OperationNotPermittedException("You cannot return a book that you do not own");
         }
         BookTransactionHistory bookTransactionHistory = bookTransactionHistoryRepository.findByBookIdAndOwnerId(bookId
                         , user.getId())
